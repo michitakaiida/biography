c7fd3880014a0e0da59422cc3b14d04cedf3f635
michi <michi@test.jp>
2017-03-02 00:57:39 +0900

    ・urlpatternsに設定がないと、ImageFieldで登録できても、登録したものが参照できない

--- biography/urls.py
+++ biography/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+#add
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
 ]
