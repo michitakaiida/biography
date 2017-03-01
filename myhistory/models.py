b50f81187e1c6ff547d0052ac307929ab7587d2e
michi <michi@test.jp>
2017-03-02 00:28:04 +0900

    モデル設定、imagefiledのために設定を追加

--- myhistory/models.py
+++ myhistory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    mission_statement = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    birthday = models.DateTimeField(blank=True, null=True)
+    dream1 = models.CharField(max_length=200, blank=True, null=True)
+    dream2 = models.CharField(max_length=200, blank=True, null=True)
+    dream3 = models.CharField(max_length=200, blank=True, null=True)
+    stronPoint1 = models.CharField(max_length=200, blank=True, null=True)
+    stronPoint2 = models.CharField(max_length=200, blank=True, null=True)
+    stronPoint3 = models.CharField(max_length=200, blank=True, null=True)
+    weekPoint1 = models.CharField(max_length=200, blank=True, null=True)
+    weekPoint2 = models.CharField(max_length=200, blank=True, null=True)
+    weekPoint3 = models.CharField(max_length=200, blank=True, null=True)
+    profile_image = models.ImageField(upload_to='images/')
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
