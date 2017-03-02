6dbd2514a2f5592c5f1febd99aeeefc2979e15f4
michi <michi@test.jp>
2017-03-02 23:45:48 +0900

    DBからの値の表示 画像の表示の練習

--- myhistory/views.py
+++ myhistory/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
 def mypage(request):
+    #後で、filterはログインIDに変える
+    my_prifile = Profile.objects.get(name='michi')
 
-    return render(request, 'myhistory/mypage.html', {})
+    return render(request, 'myhistory/mypage.html', {'my_prifile': my_prifile})
