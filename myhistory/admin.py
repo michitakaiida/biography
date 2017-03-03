687010ce56bcd178504554e43fc3b6a5637cf55e
michi <michi@test.jp>
2017-03-03 22:57:09 +0900

    モデルの更新と、Adminから変更できるように、Adminに追加

--- myhistory/admin.py
+++ myhistory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile
+from .models import Profile, Event, EventType
 
 admin.site.register(Profile)
+admin.site.register(Event)
+admin.site.register(EventType)
--- myhistory/models.py
+++ myhistory/models.py
@@ -23,3 +23,29 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class EventType(models.Model):
+    event_type_name = models.CharField(max_length=200, blank=True, null=False)
+
+    def __str__(self):
+        return self.event_type_name
+
+
+class Event(models.Model):
+    event_size_choice = (
+        (1, 1),
+        (2, 2),
+        (3, 3)
+    )
+    event_type_id = models.ForeignKey(EventType, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=30, blank=True, null=True)
+    event_size = models.PositiveSmallIntegerField(choices=event_size_choice, null=False)
+    event_date = models.DateField(null=False)
+    turningpoint_flag = models.BooleanField()
+
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.event_name
