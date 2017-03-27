7bed0a1b5c1b63bc5810713d3142b4a4f12deab8
michi <michi@test.jp>
2017-03-27 23:40:39 +0900

    HTMLとCSSを変更

--- myhistory/urls.py
+++ myhistory/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
     url(r'^$', views.mypage),
     url(r'^event$', views.event_new, name='event_new'),
+    url(r'^login/$', auth_views.login, {'template_name': 'myhistory/login.html'},name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout')
 ]
