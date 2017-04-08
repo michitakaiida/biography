2e3585d67a7c527eb5a8128a09d30bb757b4fc9e
michi <michi@test.jp>
2017-04-08 23:48:02 +0900

    crispy-djangoの導入

--- biography/settings.py
+++ biography/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 # add apps
@@ -129,3 +130,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 LOGIN_REDIRECT_URL = 'mypage'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
